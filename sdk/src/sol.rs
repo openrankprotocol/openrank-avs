a0f2d86cd897233339a10d3d4255194e207cbeee
lazovicff <github.grumbling276@passinbox.com>
2025-07-03 00:18:04 +0200

    Fix sdk publish build

--- sdk/src/sol.rs
+++ sdk/src/sol.rs
@@ -4,8 +4,5 @@ sol!(
     #[allow(missing_docs)]
     #[sol(rpc)]
     OpenRankManager,
-    concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/../contracts/out/OpenRankManager.sol/OpenRankManager.json"
-    )
+    "../contracts/out/OpenRankManager.sol/OpenRankManager.json"
 );
